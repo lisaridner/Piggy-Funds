fc06560efd5288aabd9e313a4256e4dff9026877
samuel de jesus <samue@MacBook-Air.local>
2023-04-04 19:16:54 -0400

    styled the api

--- api-testing/sdjapi.js
+++ api-testing/sdjapi.js
@@ -1,68 +1,16 @@
-// var dashboard = $('#div-test')
-// //key 2vBl2k6Nzv2Cqtal2Z2kE4sVaIT2FTNv
-// var url = "https://api.polygon.io/v3/reference/tickers?active=true&apiKey=2vBl2k6Nzv2Cqtal2Z2kE4sVaIT2FTNv"
-// fetch(url)
-// .then(response => response.json())
-// .then(data => {console.log(data)
-// dashboard.empty()
-// for (var i = 0; i<= 4; i++){
-//     var name = data.results[i].name
-//     var h3 = $('<h3>').text(name)
-//     dashboard.append(h3)
-// }
-// })
-
-// var dashboard = $('#div-test')
-// var urlNews = "https://api.polygon.io/v2/reference/news?apiKey=2vBl2k6Nzv2Cqtal2Z2kE4sVaIT2FTNv"
-// var style = $('<style>').text(`
-//     .dashboard-items{
-//         display: flex;
-//         flex-direction: row;
-//         align-items: center;
-//         justify-content: center;
-//         text-align: center;
-//         margin: 5px;
-//         border: 2px solid gray;
-//         padding: 5px;
-//         width: 100%;
-
-//     }
-//     .dashboard-item img {
-//          max-width: 100%;
-//         height: auto;
-//         margin-bottom: 10px;
-//           }
-    
-// `).appendTo('head')
-// fetch(urlNews)
-// .then(response => response.json())
-// .then(data => {console.log(data)
-// dashboard.empty()
-// for (var i = 0; i<= 4; i++){
-//     var title = data.results[i].title
-//     var img = data.results[i].image_url
-//     var description = data.results[i].description
-//     var artical = data.results[i].artical_url
-//     var h3 = $('<h3>').text(title)
-//     var pic = $('<img>').attr("src", img)
-//     var detail = $('<p>').text(description)
-//     var link = $('<a>').attr('href', artical).text('Read More');
 
 
-//     var item = $('<div>').addClass('dashboard-item');
-
-//     item.append(h3, pic, detail, link);
-//     dashboard.append(item);
-// }
-// })
-
-
+ 
 
 
 
 //--------------------------------------------------------------
+var button = $('button')
+button.click(()=>{
 
 var dashboard = $('#div-test');
+dashboard.removeClass('hii')
+console.log("hiii")
 var urlNews = "https://api.polygon.io/v2/reference/news?apiKey=2vBl2k6Nzv2Cqtal2Z2kE4sVaIT2FTNv";
 
 
@@ -75,6 +23,7 @@ var style = $('<style>').text(`
     text-align: center;
     border: 2px solid gray;
     margin: 10px;
+    width: 20%;
   }
   .dashboard-item h3 {
     font-size: 20px;
@@ -83,6 +32,7 @@ var style = $('<style>').text(`
   .dashboard-item img {
     max-width: 100%;
     margin-bottom: 10px;
+    height: auto;
   }
   .dashboard-item p {
     font-size: 8px;
@@ -115,22 +65,8 @@ fetch(urlNews)
       dashboard.append(item);
     }
   })
-
+})
 
 
 
 //--------------------------------------------------------------
-// var name = "aapl"
-// var dashboard = $('#div-test')
-// //key 2vBl2k6Nzv2Cqtal2Z2kE4sVaIT2FTNv
-// var url = "https://api.polygon.io/v3/reference/tickers?active=true&apiKey=2vBl2k6Nzv2Cqtal2Z2kE4sVaIT2FTNv"
-// fetch(url)
-// .then(response => response.json())
-// .then(data => {console.log(data)
-// // dashboard.empty()
-// // for (var i = 0; i<= 4; i++){
-// //     var name = data.results[i].name
-// //     var h3 = $('<h3>').text(name)
-// //     dashboard.append(h3)
-// // }
-// })
